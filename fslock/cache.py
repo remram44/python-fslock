e725232b9c2564307b046d765b7b825a321b88f5
Remi Rampin <r@remirampin.com>
2019-09-30 21:20:14 -0400

    cache: don't leave truncated files on kill
    If the process is killed, we don't want to leave a partially-written
    file in the cache for the next process to read. So we write to a
    temporary file that we rename on success.

--- fslock/cache.py
+++ fslock/cache.py
@@ -6,26 +6,27 @@ from . import FSLockExclusive, FSLockShared
 
 
 @contextlib.contextmanager
-def cache_get_or_set(path, create_function):
+def cache_get_or_set(cache_dir, key, create_function):
     """This function is a file cache safe for multiple processes (locking).
 
     It is used like so::
 
-        # The path to be access or created
-        cache_filename = '/tmp/cache/cachekey123'
-
         # This function is called to create the entry if it doesn't exist
-        def create_entry():
-            with open(cache_filename, 'w') as fp:
+        def create_it(tmp_path):
+            # In this function, the path is locked with an exclusive lock
+            # `tmp_path` will be renamed to the cache path on success
+            with open(tmp_path, 'w') as fp:
                 fp.write('%d\n' % long_computation())
 
-        with cache_get_or_set(cache_filename, create_entry):
-            # In this with-block, the file or directory is locked with a shared
-            # lock, so it won't be changed or removed
-            with open(cache_filename) as fp:
+        with cache_get_or_set('/tmp/cache', 'key123', create_it) as entry_path:
+            # In this with-block, the path is locked with a shared lock, so it
+            # won't be changed or removed
+            with open(entry_path) as fp:
                 print(fp.read())
     """
-    lock_path = path + '.lock'
+    entry_path = os.path.join(cache_dir, key + '.cache')
+    lock_path = os.path.join(cache_dir, key + '.lock')
+    temp_path = os.path.join(cache_dir, key + '.temp')
     while True:
         with contextlib.ExitStack() as lock:
             try:
@@ -33,28 +34,37 @@ def cache_get_or_set(path, create_function):
             except FileNotFoundError:
                 pass
             else:
-                if os.path.exists(path):
+                if os.path.exists(entry_path):
                     # Entry exists and we have it locked, return it
-                    yield
+                    yield entry_path
                     return
                 # Entry was removed while we waited -- we'll try creating
 
         with FSLockExclusive(lock_path):
-            if os.path.exists(path):
+            if os.path.exists(entry_path):
                 # Cache was created while we waited
                 # We can't downgrade to a shared lock, so restart
                 continue
             else:
+                # Remove temporary file
+                if os.path.isdir(temp_path):
+                    shutil.rmtree(temp_path)
+                elif os.path.isfile(temp_path):
+                    os.remove(temp_path)
+
                 try:
                     # Cache doesn't exist and we have it locked -- create
-                    create_function()
+                    create_function(temp_path)
                 except:
                     # Creation failed, clean up before unlocking!
-                    if os.path.isdir(path):
-                        shutil.rmtree(path)
-                    elif os.path.isfile(path):
-                        os.remove(path)
+                    if os.path.isdir(temp_path):
+                        shutil.rmtree(temp_path)
+                    elif os.path.isfile(temp_path):
+                        os.remove(temp_path)
                     raise
+                else:
+                    # Rename it to destination
+                    os.rename(temp_path, entry_path)
 
                 # We can't downgrade to a shared lock, so restart
                 continue
